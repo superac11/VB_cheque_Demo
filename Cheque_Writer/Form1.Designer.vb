179d04cf1d8f453d9d917f09336c3ea6d59755ab
Super Ac <superac11@hotmail.com>
2019-10-14 11:58:29 -0400

    Initial

---
+++ Cheque_Writer/Form1.Designer.vb
@@ -0,0 +1,245 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtamt = New System.Windows.Forms.TextBox()
+        Me.btnwrite = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox6 = New System.Windows.Forms.GroupBox()
+        Me.lblamt = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox5 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.lblname = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.lbldate = New System.Windows.Forms.Label()
+        Me.lblwords = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox6.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtamt
+        '
+        Me.txtamt.Location = New System.Drawing.Point(105, 26)
+        Me.txtamt.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.txtamt.Name = "txtamt"
+        Me.txtamt.Size = New System.Drawing.Size(89, 22)
+        Me.txtamt.TabIndex = 0
+        '
+        'btnwrite
+        '
+        Me.btnwrite.Location = New System.Drawing.Point(253, 28)
+        Me.btnwrite.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnwrite.Name = "btnwrite"
+        Me.btnwrite.Size = New System.Drawing.Size(111, 31)
+        Me.btnwrite.TabIndex = 1
+        Me.btnwrite.Text = "Write Cheque"
+        Me.btnwrite.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Location = New System.Drawing.Point(42, 82)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GroupBox1.Size = New System.Drawing.Size(915, 226)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.GroupBox2.Controls.Add(Me.lblwords)
+        Me.GroupBox2.Controls.Add(Me.GroupBox6)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.GroupBox5)
+        Me.GroupBox2.Controls.Add(Me.GroupBox4)
+        Me.GroupBox2.Controls.Add(Me.lblname)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.GroupBox3)
+        Me.GroupBox2.Location = New System.Drawing.Point(6, 8)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GroupBox2.Size = New System.Drawing.Size(902, 212)
+        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.TabStop = False
+        '
+        'GroupBox6
+        '
+        Me.GroupBox6.Controls.Add(Me.lblamt)
+        Me.GroupBox6.Location = New System.Drawing.Point(644, 75)
+        Me.GroupBox6.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GroupBox6.Name = "GroupBox6"
+        Me.GroupBox6.Padding = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GroupBox6.Size = New System.Drawing.Size(178, 39)
+        Me.GroupBox6.TabIndex = 7
+        Me.GroupBox6.TabStop = False
+        '
+        'lblamt
+        '
+        Me.lblamt.AutoSize = True
+        Me.lblamt.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblamt.Location = New System.Drawing.Point(8, 10)
+        Me.lblamt.Name = "lblamt"
+        Me.lblamt.Size = New System.Drawing.Size(0, 25)
+        Me.lblamt.TabIndex = 9
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(620, 86)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 31)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "$"
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(70, 128)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 25)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "AMOUNT"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'GroupBox5
+        '
+        Me.GroupBox5.Location = New System.Drawing.Point(151, 135)
+        Me.GroupBox5.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GroupBox5.Name = "GroupBox5"
+        Me.GroupBox5.Padding = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GroupBox5.Size = New System.Drawing.Size(667, 8)
+        Me.GroupBox5.TabIndex = 5
+        Me.GroupBox5.TabStop = False
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Location = New System.Drawing.Point(151, 98)
+        Me.GroupBox4.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Padding = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GroupBox4.Size = New System.Drawing.Size(475, 8)
+        Me.GroupBox4.TabIndex = 4
+        Me.GroupBox4.TabStop = False
+        '
+        'lblname
+        '
+        Me.lblname.AutoSize = True
+        Me.lblname.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblname.Location = New System.Drawing.Point(156, 70)
+        Me.lblname.Name = "lblname"
+        Me.lblname.Size = New System.Drawing.Size(0, 29)
+        Me.lblname.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(46, 70)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 45)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "PAY TO THE ORDER  OF"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.lbldate)
+        Me.GroupBox3.Location = New System.Drawing.Point(644, 36)
+        Me.GroupBox3.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Padding = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GroupBox3.Size = New System.Drawing.Size(178, 38)
+        Me.GroupBox3.TabIndex = 1
+        Me.GroupBox3.TabStop = False
+        '
+        'lbldate
+        '
+        Me.lbldate.AutoSize = True
+        Me.lbldate.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbldate.Location = New System.Drawing.Point(5, 9)
+        Me.lbldate.Name = "lbldate"
+        Me.lbldate.Size = New System.Drawing.Size(55, 20)
+        Me.lbldate.TabIndex = 0
+        Me.lbldate.Text = "Date: "
+        '
+        'lblwords
+        '
+        Me.lblwords.AutoSize = True
+        Me.lblwords.Location = New System.Drawing.Point(158, 116)
+        Me.lblwords.Name = "lblwords"
+        Me.lblwords.Size = New System.Drawing.Size(64, 17)
+        Me.lblwords.TabIndex = 9
+        Me.lblwords.Text = ".............."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(1028, 352)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnwrite)
+        Me.Controls.Add(Me.txtamt)
+        Me.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox6.ResumeLayout(False)
+        Me.GroupBox6.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtamt As TextBox
+    Friend WithEvents btnwrite As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents lbldate As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblname As Label
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox5 As GroupBox
+    Friend WithEvents GroupBox6 As GroupBox
+    Friend WithEvents lblamt As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblwords As Label
+End Class
---
+++ Cheque_Writer/Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs) Handles GroupBox2.Enter
+
+    End Sub
+
+    Private Sub btnwrite_Click(sender As Object, e As EventArgs) Handles btnwrite.Click
+        lbldate.Text = "Date:" & DateString
+        lblname.Text = "Sagar Acharya"
+        lblamt.Text = txtamt.Text + ".00"
+        lblwords.Text = GroupToWords(txtamt.Text) + "Dollars"
+
+
+
+
+
+    End Sub
+
+
+    ' Convert a number between 0 and 999 into words.
+    Private Function GroupToWords(ByVal num As Integer) As _
+        String
+        Static one_to_nineteen() As String = {"zero", "one",
+            "Two", "Three", "Four", "Five", "Six", "Seven",
+            "Eight", "nine", "ten", "eleven", "twelve",
+            "thirteen", "fourteen", "fifteen", "sixteen",
+            "seventeen", "eightteen", "nineteen"}
+        Static multiples_of_ten() As String = {"twenty",
+            "thirty", "forty", "fifty", "sixty", "seventy",
+            "eighty", "ninety"}
+
+        ' If the number is 0, return an empty string.
+        If num = 0 Then Return ""
+
+        ' Handle the hundreds digit.
+        Dim digit As Integer
+        Dim result As String = ""
+        If num > 99 Then
+            digit = num \ 100
+            num = num Mod 100
+            result = one_to_nineteen(digit) & " hundred"
+        End If
+
+        ' If num = 0, we have hundreds only.
+        If num = 0 Then Return result.Trim()
+
+        ' See if the rest is less than 20.
+        If num < 20 Then
+            ' Look up the correct name.
+            result &= " " & one_to_nineteen(num)
+        Else
+            ' Handle the tens digit.
+            digit = num \ 10
+            num = num Mod 10
+            result &= " " & multiples_of_ten(digit - 2)
+
+            ' Handle the final digit.
+            If num > 0 Then
+                result &= " " & one_to_nineteen(num)
+            End If
+        End If
+
+        Return result.Trim()
+    End Function
+
+
+
+
+
+End Class
+
